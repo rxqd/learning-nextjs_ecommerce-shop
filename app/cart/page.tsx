0c78573830c33d2a7b642a4e447219c6518d86c8
Max Masterius <1058433+rxqd@users.noreply.github.com>
2024-11-25 21:42:40 +0200

    Show cart products

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import { useState } from "react";
+import { products } from "../product-data";
+import ProductsList from "@/components/products/ProductsList";
+
 export default function CartPage() {
-    return <h1>Cart</h1>
+    const [productIds] = useState(['123', '345']);
+    const cartProducts = products.filter(product => productIds.includes(product.id));
+
+    return (
+        <>
+            <h1>Shopping Cart</h1>
+            <ProductsList products={cartProducts} />
+        </>
+    )
 }
